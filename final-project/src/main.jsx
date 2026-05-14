93c6498b7d6d2efab90f86f1c35ade7de5b1d745
codingscrub123 <113481707+codingscrub123@users.noreply.github.com>
2026-05-14 18:15:05 +0000

    final

--- final-project/src/main.jsx
+++ final-project/src/main.jsx
@@ -8,7 +8,7 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/DGMD-E-28">
       <App />
     </BrowserRouter>
   </React.StrictMode>
